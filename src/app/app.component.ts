32c81917555a2ca2fe85d01bcc33afa9e5997536
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2019-03-26 14:40:13 +0100

    feat: menu avancé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,23 @@ export class AppComponent {
   // propriétés
 	titre = 'Pizza YOLO';
   menu = ["Vegetariana", "Diavolo", "Parmeggiano", "Poulet", "4 fromages"];
+  menuComplet = [
+    { 
+      nom: "Vegetariana", 
+      prix: 12, 
+      ingredient: ["tomate", "courgette"], 
+      image: "" 
+    }, 
+    { 
+      nom: "Diavolo", 
+      prix: 13, 
+      ingredient: ["tomate", "chorizo"], 
+      image: ""  
+    },
+    { nom: "Parmeggiano", prix: 9, ingredient: ["tomate", "parmesan"], image: "" }, 
+    { nom: "Poulet", prix: 12, ingredient: ["tomate", "poulet"], image: ""  },  
+    { nom: "4 fromages", prix: 14, ingredient: ["crème", "gorgonzola"], image: ""  }
+  ];
   menuVisible = false;
   pristine = true
   nom = "";
