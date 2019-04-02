2bd0efd585c4ed65b19dcedeed6527c843960aee
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2019-04-02 09:43:27 +0200

    feat(state2): final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { pizzas } from "./data/pizzas";
 
 @Component({
 	selector: 'app-root',
@@ -9,7 +8,38 @@ import { pizzas } from "./data/pizzas";
 export class AppComponent {
   // propriétés
 	titre = 'Pizza YOLO';
-  menuComplet = pizzas;
+  menuComplet = [
+    { 
+      nom: "Vegetariana", 
+      prix: 12, 
+      ingredients: ["tomate", "courgette"], 
+      image: "vegetariana.jpeg" 
+    }, 
+    { 
+      nom: "Diavolo", 
+      prix: 13, 
+      ingredients: ["tomate", "chorizo"], 
+      image: "diavolo.jpeg"  
+    },
+    { 
+      nom: "Parmeggiano", 
+      prix: 9, 
+      ingredients: ["tomate", "parmesan"], 
+      image: "parmegiano.jpeg" 
+    }, 
+    { 
+        nom: "4 fromages", 
+      prix: 14, 
+      ingredients: ["crème", "gorgonzola"], 
+      image: "4f.jpeg"  
+    },   
+    { 
+      nom: "Poulet", 
+      prix: 13, 
+      ingredients: ["tomate", "courgette"], 
+      image: "poulet.jpeg" 
+    }
+  ];
   menuVisible = true;
   nom = "";
   panier = [];
